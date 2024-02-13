dbceaeec42707e14e2e51c6d1b4654773d56f25a
folklorexile <3177830339@qq.com>
2024-02-13 20:18:44 +0800

    在idea中第二次提交

--- src/com/itheima/sh/a_git_01/Demo01.java
+++ src/com/itheima/sh/a_git_01/Demo01.java
@@ -3,5 +3,6 @@ package com.itheima.sh.a_git_01;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Happy Chinese new year!");
     }
 }
